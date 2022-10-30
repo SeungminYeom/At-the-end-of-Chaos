7ad4de7c20d73a7a4e1d632ec0adf51e4da9209f
BaekRyang <graywolf147@gmail.com>
2022-10-30 22:00:55 +0900

    테스트용
    Test

--- At the end of Chaos/Assets/Scripts/Networks/Lobby.cs
+++ At the end of Chaos/Assets/Scripts/Networks/Lobby.cs
@@ -15,7 +15,7 @@ public class Lobby : MonoBehaviourPunCallbacks, IPunObservable
     private string gameVersion = "1.0";
     private bool createGameEnabled = false;
 
-    private byte requiredPlayer = 3;
+    private byte requiredPlayer = 2;
 
     public string[] playerNames = new string[4];
     public GameObject[] players = new GameObject[4];
@@ -207,19 +207,21 @@ public class Lobby : MonoBehaviourPunCallbacks, IPunObservable
 
     public override void OnPlayerEnteredRoom(Player newPlayer)
     {
+        //플레이어가 들어오면 MasterClient가 플레이어 닉네임 리스트를 업데이트 해준다.
         for (int i = 1; i < 4; i++)
         {
-            Debug.Log("player" + i + " = " + players[i].activeSelf);
             if (PhotonNetwork.IsMasterClient && !players[i].activeSelf)
             {
-                Debug.Log(i + "player Entered");
                 playerNames[i] = newPlayer.NickName;
-                players[i].GetComponentInChildren<TextMesh>().text = playerNames[i];
                 break;
             }
-
         }
-        if (PhotonNetwork.CurrentRoom.PlayerCount == requiredPlayer)
+
+        //모든 플레이어가 다른 플레이어의 이름을 오브젝트에 업데이트 한다.
+        StartCoroutine(InitPlayer());
+
+        //플레이어가 MasterClient이면서 필요한 인원이 충족되면 게임시작 버튼을 Enable해준다.
+        if (PhotonNetwork.IsMasterClient && PhotonNetwork.CurrentRoom.PlayerCount == requiredPlayer)
         {
             startBtn.gameObject.SetActive(true);
         }
@@ -246,8 +248,9 @@ public class Lobby : MonoBehaviourPunCallbacks, IPunObservable
         }
     }
 
-    void InitPlayer()
+    IEnumerator InitPlayer()
     {
+        yield return null;
         for(int i = 0; i<4; i++)
         {
             if (playerNames[i] != "")
@@ -258,6 +261,7 @@ public class Lobby : MonoBehaviourPunCallbacks, IPunObservable
         }
     }
 
+    //변수 동기화
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
         if (stream.IsWriting)
@@ -267,10 +271,5 @@ public class Lobby : MonoBehaviourPunCallbacks, IPunObservable
         {
             playerNames = (string[])stream.ReceiveNext();
         }
-    }
-
-    public void FixedUpdate()
-    {
-        InitPlayer();
     }
 }
