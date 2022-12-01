d307337604060ef41a28a5b80b4be8b86f2be99e
BaekRyang <graywolf147@gmail.com>
2022-12-01 21:42:53 +0900

    Gun Firing
    좀비들이 총알에 타격되고 이에 따른 처리들
    서버 싱크 완료

--- At the end of Chaos/Assets/_Scripts/Gun.cs
+++ At the end of Chaos/Assets/_Scripts/Gun.cs
@@ -14,6 +14,7 @@ public class Gun : MonoBehaviour
     [SerializeField] float gunShootingTime = 0.2f;
     [SerializeField] LayerMask layerMask;
 
+    PhotonView pv;
     float hitOffset = 0.5f;
 
     GameObject fireLight;
@@ -28,15 +29,16 @@ public class Gun : MonoBehaviour
 
     RaycastHit hit;
     GameObject testSp;
-    Ray r;
+    Ray r = new Ray();
     Transform gun;
     Vector3 gunPos;
     Vector3 hitOffsetPos;
 
     BulletTrailManager bulletTrailManager;
-  
+    LineRenderer targetingLazer;
 
-
+    [SerializeField] Gradient r2b;
+    [SerializeField] Gradient dr2b;
     GunType typeOnHand
     {
         get { return _typeOnHand; }
@@ -51,6 +53,8 @@ public class Gun : MonoBehaviour
 
     void Start()
     {
+        pv = GetComponent<PhotonView>();
+
         fireLight = transform.Find("FireLight").gameObject;
         pistolFireTransform = transform.Find("Pistol").GetChild(0);
         shotgunFireTransform = transform.Find("Shotgun").GetChild(0);
@@ -63,96 +67,103 @@ public class Gun : MonoBehaviour
         StartCoroutine(Reload());
 
         testSp = GameObject.Find("Target");
-        gun = GameServerManager.instance.player.transform.Find("Pistol");
-        gunPos = gun.GetChild(0).position;
+        
 
         bulletTrailManager = GameObject.Find("BulletTrailsPool").GetComponent<BulletTrailManager>();
+        targetingLazer = GetComponent<LineRenderer>();
     }
 
     void Update()
     {
-        Transform gun = GameServerManager.instance.player.transform.Find("Pistol"); //총의 Transform을 얻어와 저장
-        Vector3 gunPos = gun.GetChild(0).position;
-        Vector3 linePos = new Vector3(gunPos.x, 0f, gunPos.z); //좀비는 아래에 있으니깐 아래에서 쏜다.
-        if (Physics.Raycast(linePos, transform.forward, out hit, range, layerMask))
+        if (pv.IsMine)
         {
-            hitOffsetPos = hit.collider.transform.position + Vector3.up * hitOffset;
-            Debug.DrawRay(gunPos, hitOffsetPos - gunPos, Color.red);
-            //타겟이 있으면 좀비를 타겟으로 하여 표시해준다.
-            testSp.transform.position = hit.collider.transform.position;
-            gun.LookAt(hitOffsetPos);
-            //총도 대상을 바라본다.
+            gun = gameObject.transform.Find("Pistol");
+            gunPos = gun.GetChild(0).position;
+            Vector3 linePos = gunPos;
+            linePos.y = 0; //좀비는 아래에 있으니깐 아래에서 판정선을 쏜다.
+            targetingLazer.SetPosition(0, gunPos);
+            r.origin = transform.position;
 
-            if (GameManager.instance.timeState == TimeState.night && rounds > 0)
+            if (
+                GameManager.instance.timeState == TimeState.night &&
+                rounds > 0 &&
+                Physics.Raycast(linePos, transform.forward, out hit, range, layerMask)
+                )
             {
+                r.direction = hit.point;
+                hitOffsetPos = hit.collider.transform.position + Vector3.up * hitOffset;
+                targetingLazer.colorGradient = r2b;
+                targetingLazer.SetPosition(1, hitOffsetPos);
+                //Debug.DrawRay(gunPos, hitOffsetPos - gunPos, Color.red);
+                //타겟이 있으면 좀비를 타겟으로 하여 표시해준다.
+                testSp.transform.position = hitOffsetPos;
+                gun.LookAt(hitOffsetPos);
+                //총도 대상을 바라본다.
+
                 if (CrossPlatformInputManager.GetButtonDown("Shoot"))
                 {
-                    bulletTrailManager.pv.RPC("PlayEffect", RpcTarget.All, gunPos, hit.collider.transform.position);
+                    Debug.Log("T");
+                    bulletTrailManager.pv.RPC("PlayEffect", RpcTarget.All, gunPos, hitOffsetPos);
+                    pv.RPC("Shoot", Photon.Pun.RpcTarget.All, hit.collider.gameObject.GetPhotonView().ViewID);
+                }
+            }
+            else
+            {
+                r.direction = transform.forward;
+                Vector3 rangeInGround = r.GetPoint(range);
+                rangeInGround.y = 0;
+
+                testSp.transform.position = rangeInGround;
+                //Debug.DrawRay(gunPos, rangeInGround - gunPos, new Color(1, 0.2f, 0.2f, 0.5f));
+                targetingLazer.colorGradient = dr2b;
+                targetingLazer.SetPosition(1, rangeInGround);
+
+                gun.LookAt(rangeInGround);
+
+                if (CrossPlatformInputManager.GetButtonDown("Shoot"))
+                {
+                    bulletTrailManager.pv.RPC("PlayEffect", RpcTarget.All, gunPos, rangeInGround);
                     //gameObject.GetComponent<PlayerMovement>().pv.RPC("Shoot", Photon.Pun.RpcTarget.All);
                 }
             }
-
-        } else {
-            r = new Ray(transform.position, transform.forward);
-            Vector3 rangeInGround = new Vector3(r.GetPoint(range).x, 0, r.GetPoint(range).z);
-
-            testSp.transform.position = rangeInGround;
-            Debug.DrawRay(gunPos, rangeInGround - gunPos, new Color(1, 0.2f, 0.2f, 0.5f));
-
-            gun.LookAt(rangeInGround);
         }
-        
-        
     }
 
-    [PunRPC]
-    public void Shoot()
+    [PunRPC] public void Shoot(int _target)
     {
+        float damage;
+        float knockbackMul;
+        GameObject target = PhotonView.Find(_target).gameObject;
+        Vector3 knockBack = target.transform.position - transform.position;
+        knockBack.y = 0;
         switch (typeOnHand)
         {
             case GunType.pistol:
-                //Debug.Log("pistol " + GunManager.instance.gunDamage.ToString());
-                if (enemyFinder.target != null)
-                {
-                    StartCoroutine(FireVFX(pistolFireTransform.position, GunManager.instance.gunDamage, true));
-                }
-                else
-                {
-                    StartCoroutine(FireVFX(pistolFireTransform.position, 0, false));
-                }
+                damage = 3f;
+                knockbackMul = 1f;
+                
+                target.GetComponent<Zombie>().pv.RPC("AttackFromPlayer", RpcTarget.All, damage, 0f, knockBack * knockbackMul);
                 break;
             case GunType.shotgun:
                 //Debug.Log("shotgun " + GunManager.instance.gunDamage.ToString());
-                if (enemyFinder.target != null)
-                {
-                    StartCoroutine(FireVFX(shotgunFireTransform.position, GunManager.instance.gunDamage, true));
-                }
-                else
-                {
-                    StartCoroutine(FireVFX(shotgunFireTransform.position, 0, false));
-                }
+                damage = 3f;
+                knockbackMul = 1f;
+
+                target.GetComponent<Zombie>().pv.RPC("AttackFromPlayer", RpcTarget.All, damage, 0f, knockBack * knockbackMul);
                 break;
             case GunType.sniperRifle:
                 //Debug.Log("sniperRifle " + (GunManager.instance.gunDamage * 2).ToString());
-                if (enemyFinder.target != null)
-                {
-                    StartCoroutine(FireVFX(sniperRifleFireTransform.position, GunManager.instance.gunDamage * 3, true));
-                }
-                else
-                {
-                    StartCoroutine(FireVFX(sniperRifleFireTransform.position, 0, false));
-                }
+                damage = 3f;
+                knockbackMul = 1f;
+
+                target.GetComponent<Zombie>().pv.RPC("AttackFromPlayer", RpcTarget.All, damage, 0f, knockBack * knockbackMul);
                 break;
             case GunType.assaultRifle:
                 //Debug.Log("assaultRifle " + (GunManager.instance.gunDamage * 0.5f).ToString());
-                if (enemyFinder.target != null)
-                {
-                    StartCoroutine(FireVFX(assaultRifleFireTransform.position, GunManager.instance.gunDamage, true));
-                }
-                else
-                {
-                    StartCoroutine(FireVFX(assaultRifleFireTransform.position, 0, false));
-                }
+                damage = 3f;
+                knockbackMul = 1f;
+
+                target.GetComponent<Zombie>().pv.RPC("AttackFromPlayer", RpcTarget.All, damage, 0f, knockBack * knockbackMul);
                 break;
         }
 
@@ -161,14 +172,70 @@ public class Gun : MonoBehaviour
             StartCoroutine(Reload());
         }
     }
+    //}public void Shoot()
+    //{
+    //    switch (typeOnHand)
+    //    {
+    //        case GunType.pistol:
+    //            //Debug.Log("pistol " + GunManager.instance.gunDamage.ToString());
+    //            if (enemyFinder.target != null)
+    //            {
+    //                StartCoroutine(FireVFX(pistolFireTransform.position, GunManager.instance.gunDamage, true));
+    //            }
+    //            else
+    //            {
+    //                StartCoroutine(FireVFX(pistolFireTransform.position, 0, false));
+    //            }
+    //            break;
+    //        case GunType.shotgun:
+    //            //Debug.Log("shotgun " + GunManager.instance.gunDamage.ToString());
+    //            if (enemyFinder.target != null)
+    //            {
+    //                StartCoroutine(FireVFX(shotgunFireTransform.position, GunManager.instance.gunDamage, true));
+    //            }
+    //            else
+    //            {
+    //                StartCoroutine(FireVFX(shotgunFireTransform.position, 0, false));
+    //            }
+    //            break;
+    //        case GunType.sniperRifle:
+    //            //Debug.Log("sniperRifle " + (GunManager.instance.gunDamage * 2).ToString());
+    //            if (enemyFinder.target != null)
+    //            {
+    //                StartCoroutine(FireVFX(sniperRifleFireTransform.position, GunManager.instance.gunDamage * 3, true));
+    //            }
+    //            else
+    //            {
+    //                StartCoroutine(FireVFX(sniperRifleFireTransform.position, 0, false));
+    //            }
+    //            break;
+    //        case GunType.assaultRifle:
+    //            //Debug.Log("assaultRifle " + (GunManager.instance.gunDamage * 0.5f).ToString());
+    //            if (enemyFinder.target != null)
+    //            {
+    //                StartCoroutine(FireVFX(assaultRifleFireTransform.position, GunManager.instance.gunDamage, true));
+    //            }
+    //            else
+    //            {
+    //                StartCoroutine(FireVFX(assaultRifleFireTransform.position, 0, false));
+    //            }
+    //            break;
+    //    }
+
+    //    if (--rounds <= 0)
+    //    {
+    //        StartCoroutine(Reload());
+    //    }
+    //}
 
     //���� Ȱ��ȭ�� �� �� �Լ��� ȣ����
-    public void EquipGun(GunType t)
-    {
-        typeOnHand = t;
-        rounds = GunManager.instance.GetGunRounds((int)typeOnHand);
-        range = GunManager.instance.GetGunRange((int)typeOnHand);
-    }
+
+    //public void EquipGun(GunType t)
+    //{
+    //    typeOnHand = t;
+    //    rounds = GunManager.instance.GetGunRounds((int)typeOnHand);
+    //    range = GunManager.instance.GetGunRange((int)typeOnHand);
+    //}
 
     IEnumerator Reload()
     {
@@ -176,23 +243,23 @@ public class Gun : MonoBehaviour
         rounds = GunManager.instance.GetGunRounds((int)typeOnHand);
     }
 
-    IEnumerator FireVFX(Vector3 firePos, float damage, bool isEnemy)
-    {
-        fireLight.SetActive(true);
-        fireLight.transform.position = firePos;
-        bulletLine.enabled = true;
-        bulletLine.SetPosition(0, firePos);
-        //bulletLine.SetPosition(1, transform.forward * 10f);
-        bulletLine.SetPosition(1, transform.position + transform.forward * 10f);
-        if (isEnemy)
-        {
-            bulletLine.SetPosition(1, enemyFinder.target.position);
-            Vector3 knockBack = enemyFinder.target.position - transform.position;
-            knockBack.y = 0;
-            enemyFinder.target.gameObject.GetComponent<Zombie>().pv.RPC("AttackFromPlayer", RpcTarget.All, damage, 0f, knockBack);
-        }
-        yield return new WaitForSeconds(gunShootingTime);
-        bulletLine.enabled = false;
-        fireLight.SetActive(false);
-    }
+    //IEnumerator FireVFX(Vector3 firePos, float damage, bool isEnemy)
+    //{
+    //    fireLight.SetActive(true);
+    //    fireLight.transform.position = firePos;
+    //    bulletLine.enabled = true;
+    //    bulletLine.SetPosition(0, firePos);
+    //    //bulletLine.SetPosition(1, transform.forward * 10f);
+    //    bulletLine.SetPosition(1, transform.position + transform.forward * 10f);
+    //    if (isEnemy)
+    //    {
+    //        bulletLine.SetPosition(1, enemyFinder.target.position);
+    //        Vector3 knockBack = enemyFinder.target.position - transform.position;
+    //        knockBack.y = 0;
+    //        enemyFinder.target.gameObject.GetComponent<Zombie>().pv.RPC("AttackFromPlayer", RpcTarget.All, damage, 0f, knockBack);
+    //    }
+    //    yield return new WaitForSeconds(gunShootingTime);
+    //    bulletLine.enabled = false;
+    //    fireLight.SetActive(false);
+    //}
 }
