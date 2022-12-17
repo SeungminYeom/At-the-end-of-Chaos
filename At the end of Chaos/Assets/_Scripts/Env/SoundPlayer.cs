c433a44ca1261828db731f8443cd6bbf63a30d51
BaekRyang <graywolf147@gmail.com>
2022-12-17 11:04:17 +0900

    BGM Engine
    배경음악 재생/크로스페이드 엔진 생성

--- At the end of Chaos/Assets/_Scripts/Env/SoundPlayer.cs
+++ At the end of Chaos/Assets/_Scripts/Env/SoundPlayer.cs
@@ -1,6 +1,13 @@
+using Photon.Pun.Demo.Cockpit;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements.Experimental;
+
+using Random = UnityEngine.Random;
 
 public class SoundPlayer : MonoBehaviour
 {
@@ -27,6 +34,19 @@ public class SoundPlayer : MonoBehaviour
     public AudioClip[] sniperIM;
     
     public AudioClip[] TrainAttacked;
+
+    public AudioClip[] backgroundMusics;
+
+    float time = 0f;
+
+    byte nowPlayer = 0;
+    GameObject audioPlayerAnchor;
+    Dictionary<string, AudioClip> audioClipDictionary;
+    AudioSource previousSource;
+    int playTime;
+
+    int fadeTime = 10;
+
     void Awake()
     {
         instance = this;
@@ -35,10 +55,69 @@ public class SoundPlayer : MonoBehaviour
         pistolRM = Resources.LoadAll<AudioClip>("Sounds/GunSound/Pistol/RemoveMag");
         pistolIM = Resources.LoadAll<AudioClip>("Sounds/GunSound/Pistol/InsertMag");
         TrainAttacked = Resources.LoadAll<AudioClip>("Sounds/TrainSound/Attacked");
+        backgroundMusics = Resources.LoadAll<AudioClip>("Sounds/Environment/ImminentThreat");
+    }
+
+    void Start()
+    {
+        audioClipDictionary = new Dictionary<string, AudioClip>();
+
+        audioPlayerAnchor = GameObject.Find("BGM");
+        for (int i = 0; i < backgroundMusics.Length; i++)
+        {
+            audioClipDictionary.Add(backgroundMusics[i].name, backgroundMusics[i]);
+        }
+
+        BGMChange("CharacterSelect");
+
+        Invoke("Test", 12);
+    }
+
+    void Test()
+    {
+        BGMChange("Night");
     }
 
     public void PlaySound(AudioClip[] _clip, Vector3 _pos)
     {
         AudioSource.PlayClipAtPoint(_clip[Random.Range(0, _clip.Length)], _pos);
+    }
+
+    public void BGMChange(string _BGMName)
+    {
+        nowPlayer++;
+        GameObject audio = new GameObject("Audio" + nowPlayer, typeof(AudioSource));
+        audio.transform.SetParent(audioPlayerAnchor.transform);
+        AudioSource source = audio.GetComponent<AudioSource>();
+
+        AudioClip clip;
+        audioClipDictionary.TryGetValue(_BGMName, out clip);
+
+        source.loop = true;
+        source.volume = 0;
+        source.clip = clip;
+
+        StartCoroutine(Fader(source));
+    }
+
+    IEnumerator Fader(AudioSource _source)
+    {
+        time = 0;
+        
+        _source.Play();
+        while (_source.volume != 1)
+        {
+            time += Time.deltaTime / fadeTime;
+
+            _source.volume = Mathf.Lerp(0, 1, time);
+            if (previousSource)
+            {
+                previousSource.volume = Mathf.Lerp(1, 0, time);
+                Destroy(previousSource.gameObject, fadeTime);
+            }
+            yield return null;
+        }
+        Debug.Log("Á¾·á");
+        previousSource = _source;
     }
 }
